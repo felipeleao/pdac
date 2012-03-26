d61894ab9bf224c29654db012b0c70e40c5cc8c6
felipeleao88 <felipeleao88@3cf997ab-ea96-4d62-b1ee-27a902dde618>
2012-03-26 18:53:50 +0000

    Importação do projeto

---
+++ trunk/PdacJaxRPC/ServicoHelloNoPropRPC/build/jws/.src/br/uniriotec/propid/rpc/noprop/ServicoHelloNoPropPortType.java
@@ -0,0 +1,15 @@
+package br.uniriotec.propid.rpc.noprop;
+
+/**
+ * Generated interface, please do not edit.
+ * Date: [Mon Mar 12 15:25:47 BRT 2012]
+ */
+
+public interface ServicoHelloNoPropPortType extends java.rmi.Remote {
+
+  /**
+   * Web Method: hello ...
+   */
+  java.lang.String hello()
+      throws java.rmi.RemoteException;
+}
